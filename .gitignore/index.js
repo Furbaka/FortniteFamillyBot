6962d92522c84d39cb3a159d3243665d8b280e6b
Mistigrix <37482633+Mistigrix@users.noreply.github.com>
2018-04-07 13:31:20 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -13,7 +13,18 @@ bot.on('guildMemberAdd', member => {
     member.sendMessage("Salut ! Je suis le BOT de la Fortnite Family , tu est le bienvenue ! Je t'invite à lire le règlement , c'est moi qui te passeras toutes les infos et peut être tes avertissement !");
 });
 
-bot.on('message', message => {
-
+client.on("message", (message) => {
+    if (message.content.startsWith("F)kick")) {
+        // Easy way to get member object though mentions.
+        var member= message.mentions.members.first();
+        // Kick
+        member.kick().then((member) => {
+            // Successmessage
+            message.channel.send(":wave: " + member.displayName + " a bien été kick :point_right: ");
+        }).catch(() => {
+             // Failmessage
+            message.channel.send("Nope !");
+        });
+    }
 });
 
